a86d32e83baf91e8e3972243a4018d3a3520b9b1
ppegoretti <ppegoretti@hotmail.com>
2022-06-24 07:55:53 -0300

    pre release

--- src/App.tsx
+++ src/App.tsx
@@ -31,16 +31,32 @@ let transformPreviousRead = 0
 let maxStabilizedRead = 0
 let zeroLock = false
 let previousRead = 0
+let previousTara = 0
+
+let valueTeste = '0'
+let changeKeg = true
 
 const limitTime = 6
+let lockRepeat = false
+
+const min = 12
+const max = 20
+const arr = Array.from({ length: (max - min + 1) * 2 }, (_, i) => i / 2 + min)
 
 const calculateNewMinimumWeight = (read: number, tara: number, size: number): number => {
+  // valueTeste = changeKeg ? '30' : '20'
   const transformRead = Math.round((read / 1000) * 100) / 100
-  if (transformRead < tara) {
+  if (transformRead < tara || previousTara !== tara) {
     zeroLock = false
   }
 
-  if (Math.abs(transformPreviousRead - transformRead) < 0.5 && transformRead > tara) {
+  valueTeste = String(zeroLock)
+  previousTara = tara
+  if (
+    Math.abs(transformPreviousRead - transformRead) < 0.5 &&
+    transformRead > tara &&
+    !zeroLock
+  ) {
     timeOutStabilized += 1
     if (timeOutStabilized > limitTime) {
       zeroLock = true
@@ -50,7 +66,6 @@ const calculateNewMinimumWeight = (read: number, tara: number, size: number): nu
   } else {
     timeOutStabilized = 0
   }
-
   transformPreviousRead = transformRead
 
   if (!zeroLock) {
@@ -78,12 +93,33 @@ setInterval(() => {
   )
 
   rawRead = Math.round(readingsArray[count])
-  count += 1
+  if (!lockRepeat) {
+    count += 1
+  }
 }, timeToUpdateTemperature)
+
+setInterval(() => {
+  if (lockRepeat) {
+    lockRepeat = false
+  }
+}, 2000)
 
 // canvas logs history
 const logs: number[] = []
 const logsWeights: number[] = []
+
+function changeKegButton() {
+  changeKeg = !changeKeg
+  if (changeKeg) {
+    kegType.tara = 10
+    kegType.size = 30
+  }
+  if (!changeKeg) {
+    kegType.tara = 7.5
+    kegType.size = 20
+  }
+  lockRepeat = !lockRepeat
+}
 
 export function App() {
   const setup = (p5: P5, canvasParentRef: Element) => {
@@ -91,6 +127,11 @@ export function App() {
     p5.background(...bgColor)
       .textSize(28)
       .textAlign('left')
+    p5.createButton('Troca Barril')
+      .size(150, 80)
+      .position(0, 0)
+      .mouseClicked(changeKegButton)
+      .style('text-aling:center;font-size:25px')
   }
 
   const draw = (p5: P5) => {
@@ -107,19 +148,29 @@ export function App() {
     logsWeights.push(rawWeight)
 
     createHistory(p5, logs, 'orangered', [
-      40000,
-      10000,
+      (kegType.size + kegType.tara) * 1000,
+      kegType.tara * 1000,
       borderHeight,
       canvasHeight - borderHeight,
     ])
-    createHistory(p5, logsWeights, 'cyan', [30, 0, borderHeight, canvasHeight - borderHeight])
+    createHistory(p5, logsWeights, 'cyan', [
+      kegType.size,
+      0,
+      borderHeight,
+      canvasHeight - borderHeight,
+    ])
 
     createDots(
       p5,
       logs,
       logsWeights,
-      [40000, 10000, borderHeight, canvasHeight - borderHeight],
-      [30, 0, borderHeight, canvasHeight - borderHeight],
+      [
+        (kegType.size + kegType.tara) * 1000,
+        kegType.tara * 1000,
+        borderHeight,
+        canvasHeight - borderHeight,
+      ],
+      [kegType.size, 0, borderHeight, canvasHeight - borderHeight],
     )
 
     p5.fill(...orange)
@@ -127,7 +178,7 @@ export function App() {
       .fill(...blue)
       .text(customLog3(rawWeight), logsPos.log2, 32)
       .fill(...white)
-      .text(previousRead, logsPos.log3, 32)
+      .text(valueTeste, logsPos.log3, 32)
 
       .fill(...blue)
       .text(`${customLog(rawWeight)}L`, logsPos.log4, 32)
--- src/createLongArray.ts
+++ src/createLongArray.ts
@@ -1,6 +1,8 @@
-export const createLongArray = (max1 = 39000, max2 = 42000, min = 5000) => {
+import { clamp } from './utils'
+
+export const createLongArray = (max1 = 39000, max2 = 42000, max3 = 25000, min = 5000) => {
   const initial1 = new Array(15).fill(0).map((_, i) => Math.random() * 1000 * 0.1 + max1)
-  const updating1a = new Array(15).fill(0).map((_, i) => max1 - i * 1000)
+  const updating1a = new Array(15).fill(0).map((_, i) => clamp(max1 - i * 1000, 35000, max1))
   const updating1b = new Array(10).fill(0).map((_, i) => max1 / 1.5 + i * 1000)
   const updating1c = new Array(10).fill(0).map((_, i) => max1 / 1.1 - i * 1000)
   const updating1d = new Array(18).fill(0).map((_, i) => max1 / 1.5 - i * 1000)
@@ -8,13 +10,26 @@ export const createLongArray = (max1 = 39000, max2 = 42000, min = 5000) => {
   const refreshing1 = new Array(25).fill(0).map((_, i) => min + i * 1500)
 
   const initial2 = new Array(15).fill(0).map((_, i) => Math.random() * 1000 * 0.1 + max2)
-  const updating2a = new Array(20).fill(0).map((_, i) => max2 - i * 1000)
+  const updating2a = new Array(20).fill(0).map((_, i) => clamp(max2 - i * 1000, 32000, max2))
   const updating2b = new Array(10).fill(0).map((_, i) => max2 / 1.8 + i * 1000)
   const updating2c = new Array(30).fill(0).map((_, i) => max2 / 1.3 - i * 1000)
   const replacing2 = new Array(25).fill(0).map((_, i) => Math.random() * 1000 * 0.1 + min)
   const refreshing2 = new Array(25).fill(0).map((_, i) => min + i * 1500)
 
+  const initial3 = new Array(15).fill(0).map((_, i) => Math.random() * 1000 * 0.1 + max3)
+  const updating3a = new Array(20).fill(0).map((_, i) => clamp(max3 - i * 1000, 14000, max3))
+  const updating3b = new Array(10).fill(0).map((_, i) => max3 / 1.8 + i * 1000)
+  const updating3c = new Array(30).fill(0).map((_, i) => max3 / 1.3 - i * 1000)
+  const replacing3 = new Array(25).fill(0).map((_, i) => Math.random() * 1000 * 0.1 + min)
+  const refreshing3 = new Array(25).fill(0).map((_, i) => min + i * 1500)
+
   return [
+    ...initial3,
+    ...updating3a,
+    ...updating3b,
+    ...updating3c,
+    ...replacing3,
+    ...refreshing3,
     ...initial1,
     ...updating1a,
     ...updating1b,
